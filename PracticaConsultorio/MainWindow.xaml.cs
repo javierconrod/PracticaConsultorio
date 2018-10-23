35f70016d4f0d12ccf9375ec440ed147dffba5ac
Javier Contreras <javierin_kl@hotmail.com>
2018-10-23 08:35:29 -0700

    Agregar archivos de proyecto.

---
+++ PracticaConsultorio/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PracticaConsultorio
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            Paciente nuevoPaciente = new Paciente();
+            nuevoPaciente.Nombre = txtNombre.Text;
+            nuevoPaciente.Direccion = txtDireccion.Text;
+            nuevoPaciente.Telefono = txtTelefono.Text;
+            nuevoPaciente.Edad = int.Parse(txtEdad.Text);
+            nuevoPaciente.Altura = float.Parse(txtAltura.Text);
+            nuevoPaciente.Peso = float.Parse(txtPeso.Text);
+            nuevoPaciente.EnfermedadesCronicas = txtEnfermedadesCronicas.Text;
+
+            Datos.pacientes.Add(nuevoPaciente);
+            actualizarListaPaciente();
+
+            txtNombre.Text = "";
+            txtDireccion.Text = "";
+            txtTelefono.Text = "";
+            txtEdad.Text = "";
+            txtPeso.Text = "";
+            txtAltura.Text = "";
+            txtEnfermedadesCronicas.Text = "";
+
+            gridNuevoPaciente.Visibility = Visibility.Collapsed;
+        }
+
+        private void actualizarListaPaciente()
+        {
+            lstPacientes.Items.Clear();
+            foreach(Paciente paciente in Datos.pacientes)
+            {
+                lstPacientes.Items.Add(
+                    new ListBoxItem()
+                    {
+                        Content = paciente.Nombre
+                    }
+                );
+            }
+        }
+
+        private void btnNuevoPaciente_Click(object sender, RoutedEventArgs e)
+        {
+            gridNuevoPaciente.Visibility = Visibility.Visible;
+        }
+    }
+}
